940971af00bd114fbc4b2594e2ba26f111925c72
ivan <rakibivan5@gmail.com>
2023-03-23 18:50:50 +0530

    23-03-2023

---
+++ app/src/main/java/com/example/blinkitclone/Fragment/MobileNumberFragment.kt
@@ -0,0 +1,55 @@
+package com.example.blinkitclone.Fragment
+
+import android.animation.ValueAnimator
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
+import androidx.navigation.fragment.findNavController
+import com.example.blinkitclone.R
+import com.example.blinkitclone.databinding.FragmentMobileNumberBinding
+
+class MobileNumberFragment : Fragment() {
+
+    private var _binding : FragmentMobileNumberBinding? = null
+    private val binding  get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMobileNumberBinding.inflate(inflater,container,false)
+
+        return binding.root
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        var valueAnimation = ValueAnimator.ofFloat(0.0f, -1.0f)
+        valueAnimation.repeatCount = ValueAnimator.INFINITE
+        valueAnimation.interpolator = LinearInterpolator()
+        valueAnimation.duration = 10000L
+
+        valueAnimation.addUpdateListener {
+
+            val progress : Float = valueAnimation.getAnimatedValue() as Float
+            val width : Float = binding.img.width.toFloat()
+            val translationX = width * progress
+            binding.img.translationX = translationX
+        }
+        // valueAnimation.start()
+
+        binding.btnContinue.setOnClickListener {
+
+            findNavController().navigate(R.id.action_mobileNumberFragment_to_homeFragment)
+        }
+
+    }
+}
